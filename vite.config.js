d8a4b288ae6499baa1d0a1bcf5b9e25713209322
kthrinath2227 <k.thrinath2227@gmail.com>
2025-10-12 18:56:54 +0530

    testing

--- vite.config.js
+++ vite.config.js
@@ -202,7 +202,7 @@ logger.error = (msg, options) => {
 }
 
 export default defineConfig({
-	 base: "/SampleDev/",
+	 base: "/",
 	customLogger: logger,
 	plugins: [
 		...(isDev ? [inlineEditPlugin(), editModeDevPlugin(), iframeRouteRestorationPlugin()] : []),
